5df69da5f6d911a05f05c3a447e87d8ad6f5adc8
eric-tong <eric.yintong@gmail.com>
2018-11-27 18:18:11 +0000

    TOG-1 Add SingleToggle

---
+++ src/components/SingleToggle.tsx
@@ -0,0 +1,10 @@
+import {Component} from "react";
+import React from "react";
+
+export default class SingleToggle extends Component {
+    render() {
+        return (
+            <div>Toggle</div>
+        );
+    }
+}
