49ecf2631acf6090f881dae2433d6320a1bc27cc
eric-tong <eric.yintong@gmail.com>
2018-11-28 16:36:37 +0000

    TOG-3 Add models

---
+++ src/models/ToggleOption.ts
@@ -0,0 +1,9 @@
+export default class ToggleOption {
+    correctAnswer: string;
+    incorrectAnswer: string;
+
+    constructor(correctAnswer: string, incorrectAnswer: string) {
+        this.correctAnswer = correctAnswer;
+        this.incorrectAnswer = incorrectAnswer;
+    }
+}
---
+++ src/models/ToggleQuestion.ts
@@ -0,0 +1,11 @@
+import ToggleOption from "./ToggleOption";
+
+export default class ToggleQuestion {
+    statement: string = "";
+    options: ToggleOption[];
+
+    constructor(statement: string, options: ToggleOption[]) {
+        this.statement = statement;
+        this.options = options;
+    }
+}
