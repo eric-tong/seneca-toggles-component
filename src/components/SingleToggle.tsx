8cc86b3c18e95899680309bb80fd5820b02b41ce
eric-tong <eric.yintong@gmail.com>
2018-11-30 16:26:02 +0000

    TOG-11 Add slider

--- src/components/SingleToggle.tsx
+++ src/components/SingleToggle.tsx
@@ -10,16 +10,23 @@ export interface SingleToggleProps {
 export default class SingleToggle extends Component<SingleToggleProps> {
     render() {
         return (
-            <div className="single-toggle">
-                {this.props.answerPair.map((answer, index) =>
-                    <SingleToggleAnswer
-                        key={index}
-                        content={answer}
-                        isActive={index == this.props.activeIndex}
-                        onClick={() => this.props.onAnswerClick(index ? 1 : 0)}
-                    />
-                )}
+            <div className="single-toggle-wrapper">
+                <div className={this.sliderClassName}/>
+                <div className="single-toggle">
+                    {this.props.answerPair.map((answer, index) =>
+                        <SingleToggleAnswer
+                            key={index}
+                            content={answer}
+                            isActive={index == this.props.activeIndex}
+                            onClick={() => this.props.onAnswerClick(index ? 1 : 0)}
+                        />
+                    )}
+                </div>
             </div>
         );
+    }
+
+    get sliderClassName() {
+        return "slider " + (this.props.activeIndex ? "right" : "left");
     }
 }
