76750cd6681cb0dda804c4fd6c6e930884e0e71c
eric-tong <eric.yintong@gmail.com>
2018-11-30 21:04:11 +0000

    TOG-14 Add dynamic color to card background when all correct

--- src/styles/TogglesCardStyles.ts
+++ src/styles/TogglesCardStyles.ts
@@ -1,12 +1,19 @@
 import {darkRedHue, darkYellowHue, lightRedHue, lightYellowHue} from "../constants/Colors";
 
 export const getTogglesCardStyle = (percentageScore: number) => {
-    const lightHueRange = lightYellowHue - lightRedHue;
-    const darkHueRange = darkYellowHue - darkRedHue;
+    if (percentageScore < 1) {
+        const lightHueRange = lightYellowHue - lightRedHue;
+        const darkHueRange = darkYellowHue - darkRedHue;
 
-    const lightHue = lightRedHue + lightHueRange * percentageScore;
-    const darkHue = darkRedHue + darkHueRange * percentageScore;
-    return {
-        backgroundImage: `linear-gradient(to bottom, hsla(${lightHue}deg, 93.9%, 68%, 0.7), hsla(${darkHue}deg, 93.2%, 53.9%, 0.69))`
+        const lightHue = lightRedHue + lightHueRange * percentageScore;
+        const darkHue = darkRedHue + darkHueRange * percentageScore;
+        return {
+            backgroundImage: `linear-gradient(to bottom, hsla(${lightHue}deg, 93.9%, 68%, 0.7), hsla(${darkHue}deg, 93.2%, 53.9%, 0.69))`
+        }
+    } else {
+        // noinspection SpellCheckingInspection
+        return {
+            backgroundImage: "linear-gradient(to bottom, #47E4c1, #07CDDD)"
+        }
     }
 };
